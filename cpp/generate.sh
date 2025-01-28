2d75591dd7c856b061212db7952d73e854165ecc
Frank Krick <frank.krick@gmail.com>
2025-01-27 20:24:17 -0500

    Initial commit

---
+++ cpp/generate.sh
@@ -0,0 +1,2 @@
+protoc -I ../proto --grpc_out=. --cpp_out=. ../proto/pmx_grpc.proto --plugin="protoc-gen-grpc=/usr/bin/grpc_cpp_plugin"
+protoc -I ../proto --grpc_out=. --cpp_out=. ../proto/port.proto --plugin="protoc-gen-grpc=/usr/bin/grpc_cpp_plugin"
